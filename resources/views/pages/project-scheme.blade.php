feffc1ce494d493798ac2b16a80a3ae3afaacebf
unknown <insaf.platform@gmail.com>
2022-09-05 22:16:26 +0300

    first commit

--- resources/views/pages/project-scheme.blade.php
+++ resources/views/pages/project-scheme.blade.php
@@ -330,28 +330,37 @@
 <section id="hero2" class="hero d-flex align-items-end">
 
 
-    <ul class="contact-us-menue list-unstyled">
-        <li>
-            <a href="/reserve-house" class="cool-btn-effect contact-reserve">
-                <span>احجز الآن</span>
-            </a>
-        </li>
-        <li>
-            <a href="tel:0568436226">
-                <i class="bi bi-phone gradient-text"></i>
+<ul class="contact-us-menue list-unstyled">
+    <li>
+      <a href="/reserve-house" class="cool-btn-effect contact-reserve">
+        <span>احجز الآن</span>
+      </a>
+    </li>
+    <li hidden>
+      <a href="tel:0568436226">
+        <i class="bi bi-phone gradient-text"></i>
 
-                <span>0568436226</span>
-            </a>
-        </li>
-        <li>
-            <a class="cool-btn-effect contact-reserve" style="font-size: 13px;" target="_blank" href="/storage/images_stduio/{{$project_page_settings->pdf_file}}">
-                <i class="contact-recall"></i>
-                <span>ملف المشروع pdf</span>
-            </a>
+        <span>0568436226</span>
+      </a>
+    </li>
+    <li>
+      <a href="tel:0568436226">
+        <div>
+          <i class="bi bi-phone gradient-text" style="font-size: 24px;"></i>
+          <i class="bi bi-whatsapp gradient-text" style="font-size: 24px;"></i>
+          <span hidden>0568436226</span>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a class="cool-btn-effect contact-reserve" style="font-size: 13px;" target="_blank" href="{{$home_settings->pdf_file}}">
+        <i class="contact-recall"></i>
+        <span>ملف المشروع pdf</span>
+      </a>
 
-        </li>
+    </li>
 
-    </ul>
+  </ul>
     <div class="container">
         <div class="row">
 
